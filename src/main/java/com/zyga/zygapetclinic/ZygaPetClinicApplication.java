b450d353b4f4b448bbeaef2e138f6935f50ed322
Your Name <you@example.com>
2021-10-12 15:07:10 +0200

    1st

---
+++ src/main/java/com/zyga/zygapetclinic/ZygaPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.zyga.zygapetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZygaPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZygaPetClinicApplication.class, args);
+    }
+
+}
